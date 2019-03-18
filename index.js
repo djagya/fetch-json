28b7c2d9e6947e00e5b60508fd480e3609325e0e
djagya <danil.kabluk@gmail.com>
2019-03-18 16:41:53 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function fetchJson(url, body, headers = {}) {
+  const fetcher =
+    typeof fetch !== "undefined" ? fetch : require("isomorphic-unfetch");
+
+  return fetcher(url, {
+    headers: {
+      "Content-Type": "application/json",
+      ...headers
+    },
+    method: body ? "post" : "get",
+    body: body ? JSON.stringify(body) : null
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error(`Server error: ${res.status}, ${res.statusText}`);
+    }
+    const contentType = res.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
+      throw new TypeError("Not JSON response type");
+    }
+
+    return res.json();
+  });
+};
